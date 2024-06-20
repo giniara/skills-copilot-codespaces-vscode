52784ddee438b713d59153588e2c5fbacdf04097
giniara <122069291+giniara@users.noreply.github.com>
2024-06-20 15:37:51 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,16 @@
+// Create web server
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// Define the port to run on
+app.set('port', 3000);
+
+// Set the path to the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
